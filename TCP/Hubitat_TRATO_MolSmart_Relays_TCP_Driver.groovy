91ba3ff50851881912c27e3424ee524bc3fba104
hhorigian <hernan@horigian.com>
2024-06-26 14:28:36 -0300

    Update Hubitat_TRATO_MolSmart_Relays_TCP_Driver.groovy

--- TCP/Hubitat_TRATO_MolSmart_Relays_TCP_Driver.groovy
+++ TCP/Hubitat_TRATO_MolSmart_Relays_TCP_Driver.groovy
@@ -59,9 +59,9 @@ command "keepalivemol"
         input name: "logEnable", type: "bool", title: "Enable debug logging", defaultValue: false
         //input name: "powerstatus", type: "string", title: "Power Status" 
     input 'logInfo', 'bool', title: 'Show Info Logs?',  required: false, defaultValue: true
-    input 'logWarn', 'bool', title: 'Show Warning Logs?', required: false, defaultValue: true
-    input 'logDebug', 'bool', title: 'Show Debug Logs?', description: 'Only leave on when required', required: false, defaultValue: true
-    input 'logTrace', 'bool', title: 'Show Detailed Logs?', description: 'Only leave on when required', required: false, defaultValue: true
+    input 'logWarn', 'bool', title: 'Show Warning Logs?', required: false, defaultValue: false
+    input 'logDebug', 'bool', title: 'Show Debug Logs?', description: 'Only leave on when required', required: false, defaultValue: false
+    input 'logTrace', 'bool', title: 'Show Detailed Logs?', description: 'Only leave on when required', required: false, defaultValue: false
     //help guide
     input name: "UserGuide", type: "hidden", title: fmtHelpInfo("Manual do Driver") 
 
